336c92868b4d35500e0f571a55c961141450a883
tribhu451 <tribhu.451@gmail.com>
2020-09-15 15:03:23 +0530

    rearranged

--- execution.sh
+++ execution.sh
@@ -4,18 +4,29 @@
 
 function hadronisation()
 {
-rm -rf therminator/fomodel/lhyquid3d/*
-echo "Now no file is there inside therminator/fomodel/lhyquid3d/ "
-cp -r hydro_output/*.xml therminator/fomodel/lhyquid3d/hypersurface.xml
-echo "moving the .xml file from hydro_output to therminator/fomodel/lhyquid3d/ "
-cd therminator
-echo "entered into therminator"
-rm -rf ./events/* 
-echo "removed all the contents of folder therminator/events/ (the folder where output will be stored)."
-nohup ./therm2_events > ../logFiles/therminator.log &
-echo "job submitted in therminator. "
-cd ..
-echo "exiting from therminator ..."
+    echo "Now no file is there inside therminator/fomodel/lhyquid3d/ "
+    rm -rf therminator/fomodel/lhyquid3d/*
+    
+    
+    echo "copying the .xml file from hydro_output/  to  therminator/fomodel/lhyquid3d/ "
+    cp -r hydro_output/*.xml therminator/fomodel/lhyquid3d/hypersurface.xml
+    
+
+    echo "entered into therminator"    
+    cd therminator
+    
+    
+    echo "cleaned the folder therminator/events/ (this is the folder where output will be stored)."
+    rm -rf ./events/* 
+    
+    
+    echo "job submitted in therminator. "
+    nohup ./therm2_events > ../logFiles/therminator.log &
+    
+    
+    echo "exiting from therminator ..."
+    cd ..
+    
 }
 
 
@@ -29,13 +40,18 @@ file0='hydro_complete_flag.txt'
 # executable file of hydro
 file1='3Dhydro'
 
+
+
+
 # [step 1] delete the flag file (if exist ...)
 if [ -f $file0 ]; then
-     rm -rf $file0
-     echo "initially there was a flag file, now it's deleted ... "
+    rm -rf $file0
+    echo "initially there was a flag file, now it's deleted ... "
 else
-     echo "there was no flag file initially"
+    echo "there was no flag file initially"
 fi
+
+
 
 
 
@@ -46,6 +62,9 @@ make
 sleep 3
 
 
+
+
+
 # [step 3] submitting the job
 if [ -f $file1 ]; then
     nohup ./$file1 > ./logFiles/hydro_run.log &
@@ -54,23 +73,21 @@ if [ -f $file1 ]; then
     count=0
     while [ $count -lt 36000 ]
     do
-       if [ -f $file0 ]; then
-           echo "hydro job completed in $count sec."
-           echo "Now going to perform hadronisation (THERMINATOR)"
-           hadronisation 
-           break
-       else
-           ((count=count+210))
-           echo "wait more 3 min. 30 sec...."
-           sleep 210
-       fi
-     done
-
+	if [ -f $file0 ]; then
+            echo "hydro job completed in $count sec."
+            echo "Now going to perform hadronisation (THERMINATOR)"
+            hadronisation 
+            break
+	else
+            ((count=count+210))
+            echo "wait more 3 min. 30 sec...."
+            sleep 210
+	fi
+    done
 else
-  # [step 3.2] if compilation error occurs...
-  echo "compilation error ..."
-  exit
-
+    # [step 3.2] if compilation error occurs...
+    echo "compilation error ..."
+    exit    
 fi
 
 
